97dd608cfc370da5dd3350d3dc18054989b921b1
Dorairaju <dorairaj512@gmail.com>
2016-10-25 10:44:37 -0500

    convert email ot lowrcase

--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-module.exports = function (sequelize, Datatypes) {
+module.exports = function(sequelize, Datatypes) {
 	return sequelize.define('user', {
 		email: {
 			type: Datatypes.STRING,
@@ -14,6 +14,17 @@ module.exports = function (sequelize, Datatypes) {
 			allowNull: false,
 			validate: {
 				len: [7,100]
+			}
+		}
+	}, {
+		hooks: {
+			beforeValidate: function(user, options) {
+				// user.email
+				if (typeof user.email === 'string') {
+
+					user.email = user.email.toLowerCase();
+
+				}
 			}
 		}
 	});
